f293e5b33c09d34fba37ae328099d40279c0be0c
oz527 <124434808+oz528@users.noreply.github.com>
2023-02-05 03:06:56 +0900

    fix

--- js/weather.js
+++ js/weather.js
@@ -14,7 +14,7 @@ function onGeoSuccess(position) {
     });
 }
 function onGeoError() {
-  alert("Can't find you. No weather for you.");
+  // alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
