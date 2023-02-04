1e11a0b7243c7ce501b992f438e8eb243fad489e
oz527 <124434808+oz528@users.noreply.github.com>
2023-02-05 03:13:06 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,7 @@ const API_KEY = "b714657dd0c3c74244f01581d5d9f1bd";
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
+  console.log(`${lat},${lon}`);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((Response) => Response.json())
@@ -14,7 +15,7 @@ function onGeoSuccess(position) {
     });
 }
 function onGeoError() {
-  // alert("Can't find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
