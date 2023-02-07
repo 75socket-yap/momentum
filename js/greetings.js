d9e2a73e281b89d4b1d5920528bbe9f238e46b28
oz527 <124434808+oz528@users.noreply.github.com>
2023-02-08 07:29:29 +0900

    fix

--- js/greetings.js
+++ js/greetings.js
@@ -1,13 +1,25 @@
+const loginContainer = document.querySelector(".login-container");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutButton = document.querySelector(".logo span:last-child");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+function onLogoutSubmit() {
+  localStorage.removeItem(USERNAME_KEY);
+  greeting.innerText = "";
+  localStorage.removeItem(TODOS_KEY);
+  toDoList.innerText = "";
+  loginContainer.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginContainer.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   painGreetings(username);
@@ -15,7 +27,9 @@ function onLoginSubmit(event) {
 
 function painGreetings(username) {
   greeting.innerText = `안녕하세요 ${username}님`;
+  loginContainer.classList.add(HIDDEN_CLASSNAME);
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginInput.value = "";
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -26,3 +40,5 @@ if (savedUsername === null) {
 } else {
   painGreetings(savedUsername);
 }
+
+logoutButton.addEventListener("click", onLogoutSubmit);
--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,66 @@
 const API_KEY = "b714657dd0c3c74244f01581d5d9f1bd";
+const weatherIconList = {
+  "01": "fas fa-sun",
+  "02": "fas fa-cloud-sun",
+  "03": "fas fa-cloud",
+  "04": "fas fa-cloud-meatball",
+  "09": "fas fa-cloud-sun-rain",
+  10: "fas fa-cloud-showers-heavy",
+  11: "fas fa-poo-storm",
+  13: "far fa-snowflake",
+  50: "fas fa-smog",
+};
+const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
+  const today = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const daily = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(today)
     .then((Response) => Response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      const weatherToday = document.querySelector(
+        ".weather__today__main__text__title"
+      );
+      const city = document.querySelector(".weather__today__main__text__city");
+      const weatherIcon = document.querySelector(".weather__today__main__icon");
+      const icon = data.weather[0].icon.substr(0, 2);
+      const humidity = document.querySelector(
+        ".weather__today__other__humidity"
+      );
+      const wind = document.querySelector(".weather__today__other__wind");
+      weatherToday.innerText = `${data.weather[0].main}/${data.main.temp}°C`;
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main}/${data.main.temp}°C`;
+      weatherIcon.innerHTML = `<i class="${weatherIconList[icon]}"></i>`;
+      humidity.innerHTML = `<i class="fa-solid fa-droplet"></i><span>${data.main.humidity}%</span>`;
+      wind.innerHTML = `<i class="fa-solid fa-wind"></i><span>${data.wind.speed}km/h</span>`;
+    });
+  fetch(daily)
+    .then((Response) => Response.json())
+    .then((data) => {
+      for (let i = 0; i < 4; i++) {
+        const weatherDailyIcon = document.querySelector(
+          `.weather__daily-summary${i} .weather__daily__main__icon`
+        );
+        const dailyWeek = document.querySelector(
+          `.weather__daily-summary${i} .weather__daily__main__day`
+        );
+        const tempMax = document.querySelector(
+          `.weather__daily-summary${i} .weather__daily__other__temp-max`
+        );
+        const tempMin = document.querySelector(
+          `.weather__daily-summary${i} .weather__daily__other__temp-min`
+        );
+        if (weatherDailyIcon) {
+          const icon = data.list[i * 8].weather[0].icon.substr(0, 2);
+          const day = new Date(data.list[i * 8].dt * 1000).getDay();
+          weatherDailyIcon.innerHTML = `<i class="${weatherIconList[icon]}"></i>`;
+          dailyWeek.innerText = `${week[day]}`;
+          tempMax.innerText = `${Math.floor(data.list[i * 8].main.temp_min)}°C`;
+          tempMin.innerText = `${Math.floor(data.list[i * 8].main.temp_min)}°C`;
+        }
+      }
     });
 }
 function onGeoError() {
